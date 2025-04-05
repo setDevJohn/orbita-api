75835001eb547f5aa3a6f3a45580897b18b725f4
Jhony Freitas <jhony00._@hotmail.com>
2025-04-05 18:49:54 -0300

    docs: Config project

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+import express, { Application } from 'express';
+import { routes } from './routes';
+// import cors from 'cors';
+import morgan from 'morgan';
+
+const allowedOrigins = [
+  process.env.WHITELIST1 as string
+]
+
+export class App {
+  public app: Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+
+    this.app.get('/', (req, res) => {
+      res.json({ message: 'Hello World!' })
+    })
+  }
+
+
+  config() {
+    // this.app.use(cors({
+    //   origin: (origin, callback) => {
+    //     if (!origin || allowedOrigins.includes(origin)){
+    //       callback(null, true);
+    //     } else {
+    //       callback(new Error('Not allowed by CORS'));
+    //     }
+    //   },
+    //   methods: ["GET", "POST", "PUT", "DELETE"],
+    //   allowedHeaders: ["Content-Type", "Authorization"],
+    //   credentials: true
+    // }));
+    
+    this.app.use(morgan('dev'));
+    this.app.use(express.json());
+    this.app.use(routes);
+  }
+
+  async start(PORT: string) {
+    this.app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  
+  }
+}
