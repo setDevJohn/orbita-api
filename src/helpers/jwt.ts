96b4af2e2e68e6b24d6879de2b8d7109a26e8b93
Jhony Freitas <jhony00._@hotmail.com>
2025-08-10 09:38:56 -0300

    feat: Create jwt utils

---
+++ src/helpers/jwt.ts
@@ -0,0 +1,38 @@
+import { AppError, HttpStatus } from "./appError";
+import jwt from "jsonwebtoken";
+
+
+interface ITokenData {
+  id: number;
+  email: string;
+  verified: boolean;
+}
+
+export function generateToken(user: ITokenData): string {
+  const SECRET_KEY = process.env.JWT_SECRET;
+
+  if (!SECRET_KEY) {
+    throw new AppError(
+      "JWT_SECRET environment variable not set.",
+      HttpStatus.INTERNAL_SERVER_ERROR
+    );
+  } 
+  return jwt.sign(user, SECRET_KEY, { expiresIn: "1h" });
+}
+
+export function verifyToken(token: string): any {
+  const SECRET_KEY = process.env.JWT_SECRET;
+  
+  try {
+    if (!SECRET_KEY) {
+      throw new AppError(
+        "JWT_SECRET environment variable not set.",
+        HttpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+    return jwt.verify(token, SECRET_KEY);
+  } catch (error) {
+    console.error('Error verifying token:', (error as Error).message);
+    return null;
+  }
+}
